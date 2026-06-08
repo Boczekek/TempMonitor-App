0515131a5cf0600a4be853f5d260e6cc8765c692
Mastowicz <168667138+Mastowicz@users.noreply.github.com>
2026-06-08 18:10:57 +0200

    Delete

--- contener things/projekt69/src/main/java/org/example/projekt69/DaneRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.projekt69;
-
-import org.example.projekt69.Dane;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DaneRepository extends JpaRepository<Dane, Long> {
-}
